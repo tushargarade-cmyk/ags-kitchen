50b8cf85b96ce191c4c96b24a47dc6f20389bb9a
tushargarade-cmyk <tushargarade@gmail.com>
2026-02-03 21:48:00 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -40,3 +40,8 @@ We’d love your feedback: ${googleReviewLink}`
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+  
+
